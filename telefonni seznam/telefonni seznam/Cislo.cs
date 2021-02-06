37b748052f5e661e23fa50c5580fbe7adcc201ac
Lukáš Runt <74490287+LRunt@users.noreply.github.com>
2021-02-06 13:44:15 +0100

    Add files via upload

---
+++ telefonni seznam/telefonni seznam/Cislo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace telefonni_seznam
+{
+    public class Cislo
+    {
+        private String jmeno;
+        private int cislo;
+
+        /**
+         * Konstruktor telefoniho cila
+         * @param jmeno jmeno osoby
+         * @param cislo cislo osoby
+         */
+        public Cislo(String jmeno, int cislo){
+            this.cislo = cislo;
+            this.jmeno = jmeno;
+        }
+
+        /**
+         * vraci jmseno
+         * @return jmeno
+         */
+        public String getJmeno()
+        {
+            return jmeno;
+        }
+
+        /**
+         * vraci cislo
+         * @return cislo
+         */
+         public int getCislo()
+        {
+            return cislo;
+        }
+
+        /**
+         * formatovany vystup
+         * @return textovou reprezentaci instance
+         */
+        public override string ToString(){
+            return string.Format("{0,-9} , {1,-10}",cislo, jmeno);
+        }
+    }
+}
